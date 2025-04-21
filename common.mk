2896fe9fa511ae9c1348a9101484d78a647cd0d8
Phạm Tiên Sinh <song.long.dai.duong@gmail.com>
2025-04-22 06:13:33 +0800

    sm8550-common: Stop shipping Xiaomi Dolby
    Significant idle CPU usage

--- common.mk
+++ common.mk
@@ -117,10 +117,6 @@ PRODUCT_COPY_FILES += \
 TARGET_PANEL_DIMENSION_HAS_EXTRA_PRECISION := true
 TARGET_USE_YCRCB_CAMERA_ENCODE := true
 
-# Dolby
-PRODUCT_PACKAGES += \
-    XiaomiDolby
-
 # DRM
 PRODUCT_PACKAGES += \
     android.hardware.drm-service.clearkey
